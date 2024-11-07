9e94d54e1509bc309e2c34aa5a8fe15a0ce838a9
happycho <zanda33@naver.com>
2024-11-07 16:33:35 +0900

    중간 커밋
    중간커밋

---
+++ packages/react/components/layout/src/Block.tsx
@@ -0,0 +1,5 @@
+import { blockStype } from "./blcok.css"
+
+export const Block = () => {
+  return <div className={blockStype}>HI</div>
+}
---
+++ packages/react/components/layout/src/blcok.css.ts
@@ -0,0 +1,7 @@
+import { vars } from "@hippods/themes"
+import { style } from "@vanilla-extract/css"
+export const blockStype = style({
+  width: "100px",
+  height: "100px",
+  backgroundColor: vars.colors.$scale.pink[900],
+})
